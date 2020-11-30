ef61993c32248cbae73f46a55ac8973847f487b7
Aman Rawat <35992750+theamanrawat@users.noreply.github.com>
2020-11-30 19:56:48 +0530

    Update quora.py

--- modules/search/quora.py
+++ modules/search/quora.py
@@ -17,20 +17,21 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from core.module import BaseModule
 import re
+import requests
 
 class Module(BaseModule):
 	
 	meta = {
 		'name': 'Quora Search',
-		'author': 'Saeeddqn',
-		'version': '0.3',
+		'author': 'Aman Rawat',
+		'version': '0.1',
 		'description': 'Search your query in the quora.com and get result.',
-		'sources': ('google', 'carrot2', 'bing'),
+		'sources': ('yahoo', 'yippy', 'bing', 'google'),
 		'options': (
 			('query', None, True, 'Query string', '-q', 'store'),
 			('limit', 1, False, 'Search limit(number of pages, default=1)', '-l', 'store'),
 			('count', 50, False, 'Number of results per page(min=10, max=100, default=50)', '-c', 'store'),
-			('engine', 'google', False, 'Engine names for search(default=google)', '-e', 'store'),
+			('engine', 'bing', False, 'Engine names for search(default=bing)', '-e', 'store'),
 			('output', False, False, 'Save output to workspace', '--output', 'store_true'),
 		),
 		'examples': ('quora -q <QUERY> -l 15 --output',)
@@ -42,11 +43,12 @@ class Module(BaseModule):
 		count = self.options['count']
 		engine = self.options['engine'].split(',')
 		q = f"site:www.quora.com {query}"
-		run = self.google(q, limit, count)
+		run = self.yahoo(q, limit, count)
 		run.run_crawl()
 		links = run.links
-		profiles = []
 		pages = run.pages
+		titles = []
+		profiles = []
 
 		if 'bing' in engine:
 			run = self.bing(q, limit, count)
@@ -59,26 +61,36 @@ class Module(BaseModule):
 				self.verbose('')
 				links.append(link)
 
-		if 'carrot2' in engine:
-			run = self.carrot2(q)
+		if 'google' in engine:
+			run = self.google(q)
 			run.run_crawl()
-			pages += run.pages
-			for item in run.json_links:
-				link = item.get('url')
-				self.verbose(item.get('title'), 'C')
-				self.verbose(f"\t{link}")
-				links.append(link)
+			links = run.links
 
-		self.alert('profiles')
+		if 'yippy' in engine:
+			run = self.yippy(f'www.quora.com {query}')
+			run.run_crawl()
+			links = run.links
+
+		
 		if links == []:
-			self.output('Without result')
+		 	self.output('Without result')
 		else:
+			self.alert('profiles')
 			for link in links:
-				link = link.replace('https://www.quora.com/profile/', '').replace('/', '')
+				link = link.replace('https://www.quora.com/profile', '').replace('/', '')
 				if re.search(r'^[\w\d_\-\/]+$', link):
 					profiles.append(link)
 					self.output(f"\t{link}", 'G')
 
-			self.alert('links')
+			self.alert('titles with links')
 			for link in links:
-				self.output(f'\t{link}')
+				if 'www.quora.com' in link and 'www.quora.com/profile' not in link:
+					title = link.replace('https://www.quora.com/', '').replace('/', '')
+					title = title.replace('-', ' ')
+					title =requests.utils.unquote(title)
+					titles.append(title)
+					self.output(f'\t{title} \n\t{link}')
+				
+
+		self.save_gather({'links': links, 'title': titles, 'profiles': profiles},
+			'search/quora', query, output=self.options.get('output'))
