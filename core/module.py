98a3cb688b2ca6b0394058eb704e8131e76f2b2f
Saeed Dehqan <31902891+saeeddhqan@users.noreply.github.com>
2021-03-05 10:14:28 -0500

    Add duckduckgo search module

--- core/module.py
+++ core/module.py
@@ -1,16 +1,13 @@
 """
 OWASP Maryam!
-
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 any later version.
-
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSEdocs.  See the
 GNU General Public License for more details.
-
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
@@ -168,16 +165,16 @@ class BaseModule(framework.Framework):
 		search = carrot2.main(self, q)
 		return search
 
+	def duckduckgo(self, q, limit=1):
+		search = duckduckgo.main(self, q, limit)
+		return search
+
 	def exalead(self, q, limit=3):
 		search = exalead.main(self, q, limit)
 		return search
 
 	def google(self, q, limit=1, count=10):
 		search = google.main(self, q, limit, count)
-		return search
-
-	def duckduckgo(self, q, limit=1, count=10):
-		search = duckduckgo.main(self, q, limit, count)
 		return search
 
 	def hunter(self, q, key, limit=100):
