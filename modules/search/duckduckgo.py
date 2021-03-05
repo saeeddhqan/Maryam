8f82d6da29edcefd703074335417daafd3adfb64
Saeed Dehqan <31902891+saeeddhqan@users.noreply.github.com>
2021-03-05 10:12:37 -0500

    Add duckduckgo search module

--- modules/search/duckduckgo.py
+++ modules/search/duckduckgo.py
@@ -1,16 +1,13 @@
 """
 OWASP Maryam!
-
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 any later version.
-
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
-
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
@@ -40,12 +37,15 @@ class Module(BaseModule):
 		count = self.options['count']
 		run = self.duckduckgo(query, limit, count)
 		run.run_crawl()
-		links = run.links
+		results = run.links_with_title
+		links = run.links_with_title
+		if abs(len(results) - len(links)) > 4:
+			results = links
 
-		if links == []:
+		if results == []:
 			self.output('Without result')
 		else:
-			for link in set(links):
-				self.output(f"\t{link}")
-				print('')
-		self.save_gather(links, 'search/duckduckgo', query, output=self.options['output'])
+			for result in results:
+				self.output(result[1], 'G')
+				self.output(f"\t{result[0]}")
+		self.save_gather(results, 'search/duckduckgo', query, output=self.options['output'])
