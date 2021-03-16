638871e37ec3352305d67aabb34af21c365f178c
Vikas Kundu <vikasraist@gmail.com>
2021-03-16 19:48:36 +0530

    Added a new module which scraps for pwned email fusing cloudscraper

---
+++ modules/osint/email_pwned.py
@@ -0,0 +1,73 @@
+"""
+OWASP Maryam!
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from core.module import BaseModule
+import re
+import cloudscraper
+
+class Module(BaseModule):
+	meta = {
+		# Complete Module Name
+		"name": "Pwned database search",
+		"author": "Vikas Kundu",
+		"version": "1",
+		"description": "Search your email for data breaches",
+		"comments": (
+			"Using XmlHttp API of haveibeenpwned.com",
+			"to get JSON data"
+		),
+		"sources": ("https://www.haveibeenpwned.com"),
+		"options": (
+			("email", None, True, "Email to search for breach", "-e", "store"),
+			("output", False, False, "Save output to workspace", "--output", "store_false"),
+		),
+        "examples": ("pwned -e <email> --output",)
+	}
+	
+	def scrap(self,email):
+		url = "https://haveibeenpwned.com/unifiedsearch/"+email
+		scraper = cloudscraper.create_scraper()
+		result = scraper.get(url)
+		if(result.text!=""):
+			return result.json()
+		else:
+			return []
+
+
+	def module_run(self):
+		pwns = {}
+		email = self.options["email"]
+		if(re.search('^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$',email)):
+			self.verbose('Searching for pwning...')  
+			pwns = self.scrap(email)
+
+			if pwns == []:
+				self.output("Not pwned", "G")
+
+			else:
+				for i in pwns["Breaches"]:
+					self.output("BreachName: %s, BreachDomain: %s" % (i["Name"], i["Domain"]), "R" )
+				
+				if pwns["Pastes"]:
+					for j in pwns["Pastes"]:
+						self.output("PasteId: %s, PasteSource: %s, PasteTitle: %s" % (j["Id"], j["Source"], j["Title"] ), "B" )
+				else:
+					self.output("No pastes available", "G")
+
+		else:
+			self.error("Invalid Email %s"%email)
+		
+		# self.save_gather({<DATA>}, "<Module-Name>", "<QUERY|DORK|HOSTNAME>", <OUTPUT-FLAG(True,False)>)
+		self.save_gather(pwns, "osint/email_pwned", email, output=self.options["output"])
+
