b9be0ca0d1cf353a2a5cb365d0d8e16024c4274f
Vikas Kundu <vikasraist@gmail.com>
2021-03-19 14:58:10 +0530

    Formatted email_pwned to new style in Maryam 2.0

--- modules/osint/email_pwned.py
+++ modules/osint/email_pwned.py
@@ -12,62 +12,63 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-from core.module import BaseModule
 import re
 import cloudscraper
 
-class Module(BaseModule):
-	meta = {
-		# Complete Module Name
-		"name": "Pwned database search",
-		"author": "Vikas Kundu",
-		"version": "1",
-		"description": "Search your email for data breaches",
-		"comments": (
-			"Using XmlHttp API of haveibeenpwned.com",
-			"to get JSON data"
-		),
-		"sources": ("https://www.haveibeenpwned.com"),
-		"options": (
-			("email", None, True, "Email to search for breach", "-e", "store"),
-			("output", False, False, "Save output to workspace", "--output", "store_false"),
-		),
-        "examples": ("pwned -e <email> --output",)
-	}
+meta = {
+	"name": "Pwned database search",
+	"author": "Vikas Kundu",
+	"version": "1.0",
+	"description": "Search your email for data breaches",
+	"comments": (
+		"Using XmlHttp API of haveibeenpwned.com",
+		"to get JSON data"
+	),
+	"sources": (["https://www.haveibeenpwned.com"]),
+	"options": (
+		("email", None, True, "Email to search for breach", "-e", "store", str),
+	),
+       "examples": ("pwned -e <email> --output",)
+}
 	
-	def scrap(self,email):
-		url = "https://haveibeenpwned.com/unifiedsearch/"+email
-		scraper = cloudscraper.create_scraper()
-		result = scraper.get(url)
-		if(result.text!=""):
-			return result.json()
-		else:
-			return []
+def scrap(email):
+	url = "https://haveibeenpwned.com/unifiedsearch/"+email
+	scraper = cloudscraper.create_scraper()
+	result = scraper.get(url)
+	if(result.text!=""):
+		return result.json()
+	else:
+		return False
 
 
-	def module_run(self):
-		pwns = {}
-		email = self.options["email"]
-		if(re.search('^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$',email)):
-			self.verbose('Searching for pwning...')  
-			pwns = self.scrap(email)
+def module_api(self):
+	output, pwns = {"Breaches":[], "Pastes":[]}, {}
+	email = self.options["email"]
+	if(re.search('^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$',email)):
+		self.verbose('Searching for pwning...')  
+		pwns = scrap(email)
 
-			if pwns == []:
-				self.output("Not pwned", "G")
-
-			else:
-				for i in pwns["Breaches"]:
-					self.output("BreachName: %s, BreachDomain: %s" % (i["Name"], i["Domain"]), "R" )
-				
-				if pwns["Pastes"]:
-					for j in pwns["Pastes"]:
-						self.output("PasteId: %s, PasteSource: %s, PasteTitle: %s" % (j["Id"], j["Source"], j["Title"] ), "B" )
-				else:
-					self.output("No pastes available", "G")
-
-		else:
-			self.error("Invalid Email %s"%email)
+		if pwns:
+			for i in pwns["Breaches"]:
+				output["Breaches"] += [ "BreachName: %s , BreachDomain: %s" % ( i["Name"], i["Domain"]) ]
+				#output["Breaches"] += [ {"BreachName":i["Name"]}, {"BreachDomain":i["Domain"]} ] #uncomment for full JSON format
 		
-		# self.save_gather({<DATA>}, "<Module-Name>", "<QUERY|DORK|HOSTNAME>", <OUTPUT-FLAG(True,False)>)
-		self.save_gather(pwns, "osint/email_pwned", email, output=self.options["output"])
+			if pwns["Pastes"]:
+				for j in pwns["Pastes"]:
+					output["Pastes"] += [ "PasteId: %s, PasteSource: %s" % (j["Id"], j["Source"]) ]
+					#output["Pastes"] += [{"PasteId": j["Id"]}, {"PasteSource": j["Source"]} ] #uncomment for full JSON format
+					
+			else:
+				output["Pastes"] = "Pastes not available for this email"
+		else:
+			output["Pastes"] = output["Breaches"] = "Email not pwned"
 
+	else:
+		self.error("Invalid Email")
+		
+	self.save_gather(output, "osint/email_pwned", email, output=self.options["output"])
+	return output
+		
+def module_run(self):
+	self.alert_results(module_api(self))		
+		
