7e09708f7a73e910b139ddaedaf5b67b4d034789
z3y50n <dim_papag@windowslive.com>
2021-03-19 16:27:38 +0200

    format output

--- modules/osint/email_pwned.py
+++ modules/osint/email_pwned.py
@@ -21,54 +21,61 @@ meta = {
 	"version": "1.0",
 	"description": "Search your email for data breaches",
 	"comments": (
-		"Using XmlHttp API of haveibeenpwned.com",
-		"to get JSON data"
+			"Using XmlHttp API of haveibeenpwned.com",
+			"to get JSON data"
 	),
 	"sources": (["https://www.haveibeenpwned.com"]),
 	"options": (
 		("email", None, True, "Email to search for breach", "-e", "store", str),
 	),
-       "examples": ("pwned -e <email> --output",)
+	"examples": ("pwned -e <email> --output",)
 }
-	
+
+
 def scrap(email):
 	url = "https://haveibeenpwned.com/unifiedsearch/"+email
 	scraper = cloudscraper.create_scraper()
 	result = scraper.get(url)
-	if(result.text!=""):
+	if(result.text != ""):
 		return result.json()
 	else:
 		return False
 
 
 def module_api(self):
-	output, pwns = {"Breaches":[], "Pastes":[]}, {}
+	output, pwns = {}, {}
 	email = self.options["email"]
-	if(re.search('^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$',email)):
-		self.verbose('Searching for pwning...')  
-		pwns = scrap(email)
 
+	if(re.search('^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$', email)):
+		self.verbose('Searching for pwning...')
+		pwns = scrap(email)
 		if pwns:
-			for i in pwns["Breaches"]:
-				output["Breaches"] += [ "BreachName: %s , BreachDomain: %s" % ( i["Name"], i["Domain"]) ]
-				#output["Breaches"] += [ {"BreachName":i["Name"]}, {"BreachDomain":i["Domain"]} ] #uncomment for full JSON format
-		
+			output["Breaches"] = [{"BreachName": i["Name"], "BreachDomain":i["Domain"]} for i in pwns["Breaches"]]
 			if pwns["Pastes"]:
-				for j in pwns["Pastes"]:
-					output["Pastes"] += [ "PasteId: %s, PasteSource: %s" % (j["Id"], j["Source"]) ]
-					#output["Pastes"] += [{"PasteId": j["Id"]}, {"PasteSource": j["Source"]} ] #uncomment for full JSON format
-					
+				output["Pastes"] = [{"PasteId": j["Id"], "PasteSource": j["Source"]} for j in pwns["Pastes"]]
 			else:
 				output["Pastes"] = "Pastes not available for this email"
 		else:
 			output["Pastes"] = output["Breaches"] = "Email not pwned"
-
 	else:
 		self.error("Invalid Email")
-		
-	self.save_gather(output, "osint/email_pwned", email, output=self.options["output"])
+
+	self.save_gather(output, "osint/email_pwned", email,
+					 output=self.options["output"])
 	return output
-		
+
+
 def module_run(self):
-	self.alert_results(module_api(self))		
-		
+	output = module_api(self)
+
+	for section in output.keys():
+		if isinstance(output[section], str):
+			self.output(output[section])
+			break	
+		elif not output:
+			break
+		headers = list(output[section][0].keys())
+		rows = []
+		for data in output[section]:
+			rows.append(list(data.values()))
+		self.table(rows, headers, section)
