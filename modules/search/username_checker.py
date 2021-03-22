36e92a3f83b041c926d5719fdb7ba182c2e8c51c
Aman Singh <amanvijaykumarsingh7792@gmail.com>
2021-03-22 02:21:48 -0400

    Username_Checker search module

--- modules/search/username_checker.py
+++ modules/search/username_checker.py
@@ -1,62 +1,67 @@
+"""
+OWASP Maryam!
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
 import re
 import os
 import json
+import concurrent.futures
 
 meta = {
 	'name': 'Username Search',
 	'author': 'Aman Singh',
 	'version': '0.1',
 	'description': 'Search your query across 100+ social networks and show the results.',
-	'sources': ('google', 'carrot2', 'bing', 'yippy', 'yahoo', 'millionshort', 'qwant', 'duckduckgo'),
 	'options': (
 		('query', None, True, 'Query string', '-q', 'store', str),
-		('limit', 1, False, 'Search limit(number of pages, default=1)', '-l', 'store', int),
-		('count', 50, False, 'Number of links per page(min=10, max=100, default=50)', '-c', 'store', int),
-		('thread', 2, False, 'The number of engine that run per round(default=2)', '-t', 'store', int),
-		('engine', 'google,carrot2', False, 'Engine names for search(default=google)', '-e', 'store', str),
+		('thread', 8, False, 'The number of sites that are being checked per round(default=8)', '-t', 'store', int),
 	),
-    'examples': ('username_checker -q <QUERY> -l 15 --output',)
+    'examples': ('username_checker -q <QUERY> --output',)
 }
 
-LINKS = []
-PAGES = ''
+output = {'sites': {}}
 
-def search(self, name, q, q_formats, limit, count):
-	global PAGES,LINKS
-	engine = getattr(self, name)
-	name = engine.__init__.__name__
-	q = f"{name}_q" if f"{name}_q" in q_formats else q_formats['default_q']
-	varnames = engine.__init__.__code__.co_varnames
-	if 'limit' in varnames and 'count' in varnames:
-		attr = engine(q, limit, count)
-	elif 'limit' in varnames:
-		attr = engine(q, limit)
-	else:
-		attr = engine(q)
+def thread(self, data, query,thread_count):
+	threadpool = concurrent.futures.ThreadPoolExecutor(max_workers=thread_count)
+	futures = (threadpool.submit(check, self, data[site]['url'].format(query), site, data) for site in data)
+	for results in concurrent.futures.as_completed(futures):
+		print(f"Found {len(output['sites'])} accounts" , end= '\r')
+	print('\n')
 
-	attr.run_crawl()
-	LINKS += attr.links
-	PAGES += attr.pages
+
+def check(self,url,site, data):
+	global output
+	try:
+		req = self.request(url)
+		for error in data[site]['error'] :
+			if (error in req.text and str(req.status_code) == data[site]['status']) :
+				self.error(f"Not fount on {site}")
+				return
+		output['sites'][site] = url
+	except Exception as e:
+		self.error(f"Not fount on {site}")
+		return
 
 def module_api(self):
 	query = self.options['query']
-	limit = self.options['limit']
-	count = self.options['count']
-	engine = self.options['engine'].split(',')
-	output = {'sites': []}
-
 	filepath = os.path.join(os.getcwd(), 'data','username_checker.json')
 	file = open(filepath)
 	data = json.loads(file.read())
+	thread(self, data, query,self.options['thread'])
 
-	for site in data:
-
-		q_formats = {
-			'default_q': data[site]['url'].format(query)
-		}
-
-		self.thread(search, self.options['thread'], engine, query, q_formats, limit, count, meta['sources'])
-	output['site'] = list(set(LINKS))
 	self.save_gather(output, 'search/username_checker', query, output=self.options.get('output'))
 	return output
 
