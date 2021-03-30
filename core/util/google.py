6d034312e9fab27f1dd0818c200c1bcee96042e7
rachejazz <divyadeepti2000@gmail.com>
2021-03-31 02:57:22 +0530

    minor regex changes for email domain correctness

--- core/util/google.py
+++ core/util/google.py
@@ -30,7 +30,7 @@ class main:
 		"""
 		self.framework = main.framework
 		self.q = q
-		self.agent = 'Mozilla/5.0 (X11; Linux x86_64; rv:68.0) Gecko/20100101 Firefox/68.0'
+		self.agent = 'Mozilla/5.0 (X11; Linux x86_64; rv:86.0) Gecko/20100101 Firefox/86.0'
 		self._pages = ''
 		self.limit = limit+1
 		self.num = count
