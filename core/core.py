b0fe491b683625f6196ae22279fb9abb6938aa7f
Rishbah Jain <rishabhjainfinal@gmail.com>
2021-04-02 03:19:02 +0530

    update maryam

--- core/core.py
+++ core/core.py
@@ -103,7 +103,7 @@ class core(cmd.Cmd):
 					return self.default(line)
 				try:
 					return func(arg)
-				except Exception as e:
+				except Exception:
 					self.print_exception()
 
 	def print_topics(self, header, cmds, cmdlen, maxcol):
@@ -616,7 +616,7 @@ class core(cmd.Cmd):
 		name = options[0].lower()
 		if name in self._global_options:
 			value = ' '.join(options[1:])
-			if self._global_options_[name][2] and not value:
+			if self._global_options_[name][2] and (not value or value == 'None'):
 				print(f"{name} is a required option.")
 				return
 			if value[:1] == '$':
@@ -637,9 +637,9 @@ class core(cmd.Cmd):
 					print(f"{name} is an int option. got {value}")
 					return
 			else:
-                        	if value == 'None':
-                        		value = None
-                        	self._global_options[name] = value
+				if value == 'None':
+					value = None
+				self._global_options[name] = value
 			print(f"{name.upper()} => {value}")
 			self._save_config(name)
 		else:
@@ -986,6 +986,5 @@ class core(cmd.Cmd):
 		return [x for x in ['off', 'on', 'status', 'all', 'from ', 'clear'] if x.startswith(text.lower())]
 
 	def complete_show(self, text, line, begidx, endidx):
-		args = line.split()
 		options = sorted(self._get_show_names())
 		return [x for x in options if x.startswith(text)]
