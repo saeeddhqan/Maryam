aede23cfcdac042fb94346cb024a7ff0fdec8487
Saeed Dehqan <31902891+saeeddhqan@users.noreply.github.com>
2021-04-17 16:56:56 -0400

    Add copyright text and fix scheme bug in iris/util

--- core/util/iris/meta_search_util.py
+++ core/util/iris/meta_search_util.py
@@ -1,3 +1,19 @@
+"""
+OWASP Maryam!
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
 
 class main:
 	def __init__(self):
@@ -18,9 +34,9 @@ class main:
 			host_split = urlib.netloc.split('.')
 			suffix = host_split[-1:]
 			if path != '':
-				return f"{'.'.join(host_split)[:100]} ... .{suffix[0]} › ..."
+				return f"{urlib.scheme}://{'.'.join(host_split)[:100]} ... .{suffix[0]} › ..."
 			else:
-				return f"{'.'.join(host_split)[:90]} ... .{suffix[0]}"
+				return f"{urlib.scheme}://{'.'.join(host_split)[:90]} ... .{suffix[0]}"
 		if path in ('', '/'):
 			return host
 		path_join = []
