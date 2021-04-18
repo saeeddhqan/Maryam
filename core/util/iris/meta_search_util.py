404e85a3b6139e74fe632b320b89e6f64bbfc3a9
Anuran Roy <76481787+anuran-roy@users.noreply.github.com>
2021-04-19 00:14:50 +0530

    Update meta_search_util.py
    Further shortened code

--- core/util/iris/meta_search_util.py
+++ core/util/iris/meta_search_util.py
@@ -5,14 +5,10 @@ class main:
 	def make_cite(self, url: 'URL String') -> 'cite':
 		default_pages = ['index']
 		details = self.framework.urlib(url)
-		print(details)
 		
-		s = f"{details.scheme}://{details.netloc}"
-		if len(details.netloc) > 30:
-			s = f"{details.scheme}://{details.netloc[:5]}...{details.netloc[-25:]}"
-	
-		p = details.path if len(details.path) > 1 else ""
-		if len(p) > 30:
-			p = f"{p[:15]}...{p[-15:]}"
-		p = p.replace("/"," › ").replace("%20"," ")
-		return f"{s}{p}"
+		p=details.path if len(details.path) > 1 else ""
+		p=p.replace("/"," › ")
+		s=f"{details.scheme}://{details.netloc}{p}"
+		if len(s) > 110:
+		    s=f"{s[:110]}..."
+		return s
