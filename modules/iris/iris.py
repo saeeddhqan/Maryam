c9d16ef427a5903fd190b9736ba734362e24a7a6
Saeed Dehqan <31902891+saeeddhqan@users.noreply.github.com>
2021-04-21 18:58:20 -0400

    Update iris.py

--- modules/iris/iris.py
+++ modules/iris/iris.py
@@ -1,16 +1,13 @@
 """
 OWASP Maryam!
-
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 any later version.
-
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
-
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
@@ -27,13 +24,15 @@ meta = {
 	'options': (
 		('query', None, True, 'Query string', '-q', 'store', str),
 		('limit', 1, False, 'Search limit(number of pages, default=1)', '-l', 'store', int),
-		('count', 10, False, 'Number of results per page(default=10)', '-c', 'store', int),
+		('count', 12, False, 'Number of results per page(default=10)', '-c', 'store', int),
 	),
 	'examples': ('iris -q <QUERY> -l 15 --output --api',)
 }
 RESULTS = []
 COUNT_CONSENSUS = None
 LIMIT_CONSENSUS = None
+COUNT = 0
+
 def thread(self, function, engines, query):
 	threadpool = concurrent.futures.ThreadPoolExecutor(max_workers=len(engines))
 	futures = (threadpool.submit(function, self, engine, query) for engine in engines)
@@ -42,36 +41,50 @@ def thread(self, function, engines, query):
 		pass
 
 def search(self, name, q):
-	global RESULTS
+	global RESULTS, COUNT
 	engine = getattr(self, name)
 	limit = LIMIT_CONSENSUS[name] or 1
-	count = COUNT_CONSENSUS[name]
+	count = COUNT_CONSENSUS[name] * 2
 	if count:
 		if name == 'google':
 			attr = engine(q, limit, count, 'legacy')
 		else:
-			attr = engine(q, limit, count)
-
+			attr = engine(q, limit)
 		attr.run_crawl()
-		if name == 'google':
-			RESULTS.append(attr.results_legacy)
+		results = attr.results
+		COUNT += len(results)
+		if results:
+			RESULTS.append(results)
+		rest()
+
+def rest(self):
+	less = COUNT-self.options['limit']
+	if less < 0:
+		less = abs(less)
+		if less <= 50:
+			limit = 1
 		else:
-			RESULTS.append(attr.results)
+			limit = 2
+			less = 50
+		attr = self.bing(q, less, limit)
+		attr.run_crawl()
+		RESULTS.append(attr.results)
 
 def module_api(self):
-	global COUNT_CONSENSUS, LIMIT_CONSENSUS
+	global COUNT_CONSENSUS, LIMIT_CONSENSUS, RESULTS
 	query = self.options['query']
 	limit = self.options['limit']
 	count = self.options['count']
-	engines = ['google', 'yahoo']
-	engines_len = len(engines)
+	engines = ['google', 'duckduckgo']
 	COUNT_CONSENSUS = self.meta_search_util.compute_count_consensus(engines, count)
 	LIMIT_CONSENSUS = self.meta_search_util.compute_count_consensus(engines, limit)
 	thread(self, search, engines, query)
+	engines_len = len(RESULTS)
 	simple_merge = []
 	for i in range(len(min(RESULTS, key=len))):
-		for e in range(len(engines)):
+		for e in range(engines_len):
 			simple_merge.append(RESULTS[e%engines_len][i])
+
 	output = {'results': simple_merge}
 	self.save_gather(output, 'iris/iris', query, output=self.options['output'])
 	return output
