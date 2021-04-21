593ca5dfec7e86c7da74051796c7b64b58bb4fa9
Saeed Dehqan <31902891+saeeddhqan@users.noreply.github.com>
2021-04-21 09:25:55 -0400

    API, opt_proc integration

--- core/initial.py
+++ core/initial.py
@@ -15,7 +15,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-__version__ = 'v2.0.5'
+__version__ = 'v2.1.0'
 import argparse
 import imp
 import os
