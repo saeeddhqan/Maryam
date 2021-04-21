a066a6cf62bf6f8cdab69dc2a1651f4b4809937f
Saeed Dehqan <31902891+saeeddhqan@users.noreply.github.com>
2021-04-21 09:24:30 -0400

    add dev and package commands

--- core/core.py
+++ core/core.py
@@ -42,7 +42,7 @@ class Colors(object):
 	Y = '\u001b[38;5;226m'
 
 class core(cmd.Cmd):
-	prompt = ">>>"
+	prompt = '>>>'
 	_global_options = {}
 	_global_options_ = {}
 	_loaded_modules = {}
@@ -57,8 +57,8 @@ class core(cmd.Cmd):
 		cmd.Cmd.__init__(self)
 		self.ruler = '-'
 		self.spacer = '  '
-		self.nohelp = f'{Colors.R}[!] No help on %s{Colors.N}'
-		self.do_help.__func__.__doc__ = '''Displays this menu'''
+		self.nohelp = f"{Colors.R}[!] No help on %s{Colors.N}"
+		self.do_help.__func__.__doc__ = 'Displays this menu'
 		self.doc_header = 'Commands (type [help|?] <topic>):'
 		self._exit = 0
 
@@ -170,10 +170,10 @@ class core(cmd.Cmd):
 		if error not in self._error_stack:
 			self._error_stack.append(error)
 
-	def output(self, line, color='N', end='', prep='', linesep=True):
+	def output(self, line, color='N', end='', prep='', linesep=True, prefix='[*]'):
 		'''Formats and presents normal output.'''
 		line = self.to_str(line)
-		line = f'{prep}{Colors.B}[*]{getattr(Colors, color.upper())} {line}\033[m'
+		line = f'{prep}{Colors.B}{prefix}{getattr(Colors, color.upper())} {line}\033[m'
 		if not line.endswith(os.linesep) and linesep:
 			line += os.linesep
 		print(line, end=end)
@@ -377,14 +377,14 @@ class core(cmd.Cmd):
 		return False
 
 	# ////////////////////////////////
-	#           HISTORY 			//
+	#           ERRORS 		//
 	# ////////////////////////////////
 
 	def _reset_error_stack(self):
 		self._error_stack.clear()
 
 	# ////////////////////////////////
-	#           HISTORY 			//
+	#           HISTORY 		//
 	# ////////////////////////////////
 
 	def _init_history(self, reborn=False, write=True):
@@ -418,7 +418,7 @@ class core(cmd.Cmd):
 
 
 	# ////////////////////////////////
-	#           OPTIONS 			//
+	#           OPTIONS 		//
 	# ////////////////////////////////
 
 	def _load_config(self):
@@ -459,7 +459,7 @@ class core(cmd.Cmd):
 			json.dump(config_data, config_file, indent=4)
 
 	# ////////////////////////////////
-	#           request                //
+	#           request             //
 	# ////////////////////////////////
 
 	def _print_prepared_request(self, prepared):
@@ -512,6 +512,256 @@ class core(cmd.Cmd):
 		# display response data
 		self._print_response(resp)
 		return resp
+
+	# ////////////////////////////////
+	#         INSTALL and DEV       //
+	# ////////////////////////////////
+
+	def do_package(self, params):
+		'''Install extensions and packages and manage them.'''
+		if not params or len(params) < 2:
+			self.help_package()
+			return
+		params = params.split()
+		mode = params.pop(0).lower()
+		if mode == 'extension':
+			next_plan = params.pop(0).lower()
+			pool = self.request(
+				'https://raw.githubusercontent.com/mexts/init/main/EXTENSIONS.json').json()
+			if next_plan == 'list':
+				self.alert('List of extensions:')
+				for ext in pool:
+					self.output(f"\t{ext}")
+			elif next_plan == 'install':
+				for name in params:
+					name = params.pop(0).lower()
+					if name not in pool:
+						self.error(f"There's no '{name}' extension name.", 'core', 'do_install')
+						continue
+					mext = self.request(f"https://raw.githubusercontent.com/mexts/init/main/exts/{name}/mext").text.split('\n')
+					mext[-1] = json.loads(mext[-1].replace("'", '"'))
+					if self._dev_running_mext(name, mext, 'install'):
+						self.output(f"{name} extension has been installed.")
+						continue
+					else:
+						self.error(f"{name} extension has not been installed.", 'core', 'do_install')
+			else:
+				self.help_package()
+		elif mode == 'repo':
+			next_plan = params.pop(0).lower()
+			pool = self.request(
+				'https://raw.githubusercontent.com/mexts/init/main/PACKAGES.json').json()
+			if next_plan == 'list':
+				self.alert('List of packages:')
+				for ext in pool:
+					self.output(f"\t{ext}")
+			elif next_plan == 'install':
+				name = params.pop(0).lower()
+				if name not in pool:
+					self.error(f"There's no '{name}' package name.", 'core', 'do_install')
+					return
+				mext = self.request(f"https://raw.githubusercontent.com/mexts/init/main/{name}/main/mext").text.split('\n')
+				mext[-1] = json.loads(mext[-1].replace("'", '"'))
+				if self._dev_running_mext(name, mext, 'install'):
+					self.output(f"{name} repository has been installed.")
+				else:
+					self.error(f"{name} repository has not been installed.", 'core', 'do_install')
+			else:
+				self.help_package()
+		else:
+			self.help_package()
+
+	def _dev_tree(self, path):
+		path = f"{path}/" if path[-1] != '/' else path
+		outcome = {}
+		for dirpath, _, files in os.walk(path, followlinks=True):
+			if '/.' not in dirpath and re.search(r'^\.', dirpath) == None:
+				dirpath = dirpath.replace(path, '')
+				for file in files:
+					if dirpath in outcome:
+						outcome[dirpath].append(file)
+					else:
+						outcome[dirpath] = [file]
+		return outcome
+
+	def _dev_create_mext(self, path):
+		mext = []
+		all_files = []
+		ext_tree = self._dev_tree(path)
+		current_tree = self._dev_tree(self.path)
+		for dirpath in ext_tree:
+			files = ext_tree[dirpath]
+			for file in files:
+				filepath = os.path.join(dirpath, file)
+				full_path = os.path.join(path, filepath)
+				all_files.append(filepath)
+				if filepath.startswith('util/'):
+					project_file = f"core/{os.path.join(dirpath, file)}"
+				if dirpath == 'util':
+					dirpath = 'core/util'
+				else:
+					project_file = filepath
+					if filepath.startswith('modules/'):
+						fopen = self._is_readable(full_path)
+						if fopen:
+							mext.insert(0, 'module ' + '.'.join(file.split('.')[:-1]))
+							ver = re.search(r"'version': '([\d\.]+)',", fopen.read())
+							if ver:
+								mext.insert(1, 'version ' + ver.group(1))
+							else:
+								self.error('No version found in the module.', 'core', '_dev_create_mext')
+								return False
+						else:
+							return False
+				if dirpath in current_tree:
+					mext.append(f"move {filepath} to {dirpath}")
+				else:
+					mext.append(f"makedir {dirpath} and add {filepath}")
+		mext.append(all_files)
+		return mext, all_files
+
+	def _dev_running_mext(self, path, mext, mode='test'):
+		'''Run mext commands'''
+		for command in mext:
+			if isinstance(command, list):
+				continue
+			command_split = command.split(' ')
+			if command_split[0] in ('version', 'module'):
+				continue
+			elif command_split[0] == 'move':
+				if len(command_split) != 4:
+					self.error(f"syntax error: {command_split}", 'core', '_dev_running_mext')
+					return False
+				file_path, direct = command_split[1], command_split[3]
+				if mode == 'install':
+					file_text = self.request(f"https://raw.githubusercontent.com/mexts/init/main/exts/{path}/{file_path}").text
+				else:
+					file_text = self._is_readable(os.path.join(path, file_path))
+					if file_text:
+						file_text = file_text.read()
+					else:
+						self.error(f"cannot open files: {file_path}", 'core', '_dev_running_mext')
+						return False
+				if file_path.startswith('util/'):
+					file_path = f"core/{file_path}"
+				file_path = os.path.join(self.path, file_path)
+				file = self._is_readable(file_path, 'w')
+				if file:
+					file.write(file_text)
+					file.close()
+				else:
+					self.error(f"cannot open files:{file_path}", 'core', '_dev_running_mext')
+					return False
+			elif command_split[0] == 'makedir':
+				if len(command_split) != 5:
+					self.error(f"syntax error. not enough argument: {command_split}", 'core', '_dev_running_mext')
+					return False
+				direct, file_path = os.path.join(self.path, command_split[1]), command_split[4]
+				if not os.path.exists(direct):
+					os.mkdir(direct)
+				if mode == 'install':
+					file_text = self.request(f"https://raw.githubusercontent.com/mexts/init/main/exts/{path}/{file_path}").text
+				else:
+					file_text = self._is_readable(os.path.join(path,  file_path))
+					if file_text:
+						file_text = file_text.read()
+					else:
+						self.error(f"cannot open files: {file_path}", 'core', '_dev_running_mext')
+						return False
+				file = self._is_readable(os.path.join(self.path, file_path), 'w')
+				if file:
+					file.write(file_text)
+					file.close()
+				else:
+					self.error(f"cannot open files:{file_path}", 'core', '_dev_running_mext')
+					return False
+			else:
+				self.error(f"syntax error: {command_split[0]}", 'core', '_dev_running_mext')
+				return False
+		return True
+
+	def _dev_extension_test(self, path, mext):
+		'''Testing the extension before pull request'''
+		run_mext = self._dev_running_mext(path, mext)
+		if not run_mext:
+			return False
+		self._reset_error_stack()
+		self.do_reload()
+		if self._error_stack != []:
+			self.error('during testing the extension, the following errors occurs.')
+			for error in self._error_stack:
+				self.error(error)
+			return False
+		return True
+
+	def _dev_install_requirements(self, reqs):
+		'''Testing the extension before pull request'''
+		if '://' in reqs:
+			reqs = '/tmp/reqs'
+			file = self._is_readable(reqs, 'w')
+			if file:
+				download = self.request(reqs)
+				if download.status_code != 200:
+					self.error('No such URL to download.', 'core', '_dev_install_requirements')
+					return False
+				file.write(download.text)
+				file.close()
+			else:
+				self.error(f"Could not create a new file: {reqs}", 'core', '_dev_install_requirements')
+				return False
+		self.do_shell(f"pip install -r {reqs}")
+		return True
+
+	def do_dev(self, params):
+		'''Development kit'''
+		if not params or len(params) < 2:
+			self.help_dev()
+			return
+		params = params.split()
+		mode = params.pop(0).lower()
+		if mode == 'extension':
+			what = params.pop(0).lower()
+			if what == 'init':
+				path = params.pop(0).lower()
+				if not os.path.exists(path):
+					self.error(f"No such directory '{path}'.", 'core', 'do_dev')
+					return
+				mext_commands, all_files = self._dev_create_mext(path)
+				if not mext_commands:
+					self.error('Cannot initialize the extension.', 'core', 'do_dev')
+					return
+				mext_path = os.path.join(path, 'mext')
+				if os.path.exists(mext_path):
+					self.verbose('mext file already exist.')
+					mext_commands = self._is_readable(mext_path).read().split('\n')
+					mext_commands[-1] = json.loads(mext_commands[-1].replace("'", '"'))
+				else:
+					self.verbose(f"Creating {mext_path} file...")
+					mext_file = self._is_readable(mext_path, 'w')
+					if not mext_file:
+						return
+					mext_file.write('\n'.join(mext_commands[:-1]))
+					mext_file.write(f"\n{str(mext_commands[-1])}")
+				self.verbose('Installing pipreqs...')
+				self.do_shell('pip install pipreqs')
+				self.verbose('Creating the requirements file with pipreqs...')
+				reqs_path = os.path.join(path, 'requirements')
+				reqs_file = self._is_readable(reqs_path, 'w')
+				if not reqs_file:
+					return
+				self.do_shell(f"pipreqs {path} --savepath {reqs_path}")
+				perm = input('[!] The test stage will change the origin project and needs to install dependencies. Continue[Y/N]? ')
+				if perm.lower() in ('y', 'yes'):
+					self.verbose('Testing...')
+					if self._dev_install_requirements(reqs_path):
+						self.error('Failed', 'core', 'do_dev')
+						return False
+					result = self._dev_extension_test(path, mext_commands)
+					if result == False:
+						self.error("the extension couldn't pass the test.", 'core', 'do_dev')
+						return
+					self.verbose('the extension has been successfully tested.')
+				self.verbose('Finished. The package is ready for pull request.')
 
 	# ////////////////////////////////
 	#           SHOW                //
@@ -685,9 +935,9 @@ class core(cmd.Cmd):
 		else:
 			for section in core._cat_module_names:
 				self.heading(section)
-				if text in core._cat_module_names[section]:
-					self.output(f"Found '{text}' under {section}")
-
+				for mod in core._cat_module_names[section]:
+					if text in mod:
+						self.output(f"\tFound '{text}' under {section}: {mod}", prefix='')
 	def do_shell(self, params):
 		'''Executes shell commands'''
 		if not params:
@@ -823,9 +1073,20 @@ class core(cmd.Cmd):
 	#             HELP              //
 	# ////////////////////////////////
 
+	def help_package(self):
+		print(getattr(self, 'do_package').__doc__)
+		print(f"{os.linesep}Usage: package extension install name1,name2,..")
+		print(f"\tpackage extension list")
+		print(f"\tpackage repo list")
+		print(f"\tpackage repo install repo-name")
+
+	def help_dev(self):
+		print(getattr(self, 'do_dev').__doc__)
+		print(f"{os.linesep}Usage: dev extension init path/to/extension")
+
 	def help_history(self):
 		print(getattr(self, 'do_history').__doc__)
-		print(f'{os.linesep}Usage: history [list|from <num>|off|on|status|all|clear]')
+		print(f"{os.linesep}Usage: history [list|from <num>|off|on|status|all|clear]")
 		print('\thistory list\tShow 50 first commands')
 		print('\thistory from <num>\tShow the last <num> commands')
 		print('\thistory off\tTurn off the history logger')
@@ -839,9 +1100,9 @@ class core(cmd.Cmd):
 		print(getattr(self, 'do_report').__doc__)
 		print(f'{os.linesep}Usage: report [<format> <filename> [<module_name> or <module_name> <query(hostname,domain name, keywords,etc)>]]')
 		print('or       : report [saved] => for show queries')
-		print('Examples : report json pdf_docs(without extention) osint/docs_search company.com')
-		print(f'           report xml pdf_docs(without extention) osint/docs_search')
-		print(f'Formats  : xml,json,csv and txt{os.linesep}')
+		print('Examples : report json pdf_docs(without suffix) osint/docs_search company.com')
+		print('           report xml pdf_docs(without suffix) osint/docs_search')
+		print(f"Formats : xml,json,csv,txt{os.linesep}")
 
 	def help_search(self):
 		print(getattr(self, 'do_search').__doc__)
@@ -886,6 +1147,12 @@ class core(cmd.Cmd):
 
 	def complete_update(self, text, line, begidx, endidx):
 		return [x for x in ['check', 'module'] if x.startswith(text.lower())]
+
+	def complete_dev(self, text, line, begidx, endidx):
+		return [x for x in ['extension', 'package'] if x.startswith(text.lower())]
+
+	def complete_package(self, text, line, begidx, endidx):
+		return [x for x in ['extension', 'repo'] if x.startswith(text.lower())]
 
 	def complete_workspace(self, text, line, begidx, endidx):
 		return [x for x in ['add', 'list', 'select'] if x.startswith(text.lower())]
