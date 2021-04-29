f18025b2733db9292d04bc47257a9da1b05f5837
Saeed Dehqan <31902891+saeeddhqan@users.noreply.github.com>
2021-04-29 13:37:24 -0400

    add classifiers

--- setup.py
+++ setup.py
@@ -16,22 +16,16 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-
 from setuptools import setup
 from setuptools import find_packages
 
 def parse_requirements():
-	reqs = []
-	for name in open('requirements').read().rsplit('\n'):
-		if name:
-			try:
-				__import__(name)
-			except:
-				reqs.append(name)
-	return reqs
+	with open('requirements') as fp:
+		return [line.strip() for line in fp if line.strip()]
+	return []
 
 setup(
-	name='OWASP Maryam',
+	name='maryam',
 	version='2.1.5',
 	url='https://github.com/saeeddhqan/Maryam',
 	author='Saeed Dehqan',
@@ -39,11 +33,20 @@ setup(
 	packages=find_packages(),
 	# package_data={'': package_files()},
 	license='GPL-V3',
+	include_package_data=True,
 	description='OWASP Maryam is a modular/optional open source framework based on OSINT and data gathering.',
 	long_description=open('README.md').read(),
 	long_description_content_type='text/markdown',
 	keywords=['OWASP', 'OSINT', 'search-engine', 'social-networks', 'Maryam'],
 	install_reqs=parse_requirements(),
-	py_modules=['module1s','core' ,'data'],
-	scripts=['maryam','scripts/script2']
+	py_modules=['module1s', 'core', 'data'],
+	scripts=['maryam'],
+	classifiers=[
+		'Programming Language :: Python :: 3.8',
+		'Development Status :: 5 - Production/Stable',
+		'License :: OSI Approved :: General Public License V3',
+		'Natural Language :: English',
+		'Operating System :: POSIX :: Linux',
+		'Environment :: Console',
+	]
 )
