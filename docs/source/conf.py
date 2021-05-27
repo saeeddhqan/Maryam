31847aa870eaf8e6708d7f2770a1dc23a48e0d36
Saeed <saeeddhqan@gmail.com>
2021-05-27 12:30:18 -0400

    integrating docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,6 +31,7 @@ release = '2.2.6'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+	'myst_parser'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
