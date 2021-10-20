ef1146057d6d89a83ba2cabc0fb09dea3b65dd6e
Saeed Dehqan <31902891+saeeddhqan@users.noreply.github.com>
2021-10-20 10:08:46 -0400

    indentation

--- setup.py
+++ setup.py
@@ -21,30 +21,30 @@ from setuptools import setup, find_packages
 requirements_file = open('requirements', 'r')
 requirements = requirements_file.readlines()
 for i, line in enumerate(requirements):
-    requirements[i] = line.strip()
+	requirements[i] = line.strip()
 
 setup(
-    name='maryam',
-    version='2.5.0-1',
-    url='https://github.com/saeeddhqan/Maryam',
-    author='Saeed Dehqan',
-    author_email='saeed.dehghan@owasp.org',
-    packages=find_packages(),
-    include_package_data=True,
-    package_data={"maryam": ['data/*']},
-    license='GPL-V3',
-    description='OWASP Maryam is a modular/optional open-source framework based on OSINT and data gathering.',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    keywords=['OWASP', 'OSINT', 'search-engine', 'social-networks', 'Maryam'],
-    scripts=['bin/maryam'],
-    install_requires=requirements,
-    classifiers=[
-        'Programming Language :: Python :: 3.8',
-        'Development Status :: 5 - Production/Stable',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Natural Language :: English',
-        'Operating System :: POSIX :: Linux',
-        'Environment :: Console',
-    ]
+	name='maryam',
+	version='2.5.0-1',
+	url='https://github.com/saeeddhqan/Maryam',
+	author='Saeed Dehqan',
+	author_email='saeed.dehghan@owasp.org',
+	packages=find_packages(),
+	include_package_data=True,
+	package_data={"maryam": ['data/*']},
+	license='GPL-V3',
+	description='OWASP Maryam is a modular/optional open-source framework based on OSINT and data gathering.',
+	long_description=open('README.md').read(),
+	long_description_content_type='text/markdown',
+	keywords=['OWASP', 'OSINT', 'search-engine', 'social-networks', 'Maryam'],
+	scripts=['bin/maryam'],
+	install_requires=requirements,
+	classifiers=[
+		'Programming Language :: Python :: 3.8',
+		'Development Status :: 5 - Production/Stable',
+		'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+		'Natural Language :: English',
+		'Operating System :: POSIX :: Linux',
+		'Environment :: Console',
+	]
 )
