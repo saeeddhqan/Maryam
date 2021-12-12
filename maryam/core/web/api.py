488d48414b1c47360f8d74c033007d4cb65fe235
moshaad7 <shaadk7865@gmail.com>
2021-12-12 14:31:53 +0530

    adding iris_cluster api

--- maryam/core/web/api.py
+++ maryam/core/web/api.py
@@ -17,19 +17,20 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import flask
 from flask import request, jsonify
+from json import dumps
 
 framework = None
 app = flask.Flask('OWASP Maryam')
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-	page = '<pre>current pages:<br>/api/modules => running modules<br>/api/framework => framework commands</pre>'
+	page = '<pre>current pages:<br>/api/modules => running modules<br>/api/framework => framework commands<br>/api/iris_cluster => iris search and cluster module'
 	return page
 
 
 @app.route('/api/', methods=['GET', 'POST'])
 def api():
-	page = '<pre>current pages:<br>/api/modules => running modules<br>/api/framework => framework commands'\
+	page = '<pre>current pages:<br>/api/modules => running modules<br>/api/framework => framework commands<br>/api/iris_cluster => iris search and cluster module'\
 		   '<br><b>/api/modules?_module=<module-name>&options[short or long]...</b>'\
 		   '<br>["meta"]: api metadata'\
 		   '<br>["meta"]["error"]: error messages. None if no error occurs'\
@@ -40,6 +41,13 @@ def api():
 		   '<br>["meta"]: api metadata'\
 		   '<br>["meta"]["error"]: error messages. None if no error occurs'\
 		   '<br>["meta"]["command"]: input command'\
+		   '<br><b>/api/iris_cluster?options[short or long]...</b>'\
+		   '<br>["meta"]: api metadata'\
+		   '<br>["meta"]["error"]: error messages. None if no error occurs'\
+		   '<br>["output"]: module output'\
+		   '<br>["output"]["iris_search_result]: search results by iris module'\
+		   '<br>["output"]["cluster_result]: search results by cluster module'\
+		   '<br>["output"]["running_errors]: error messages that occurs during running the module'\
 		   '</pre>'
 	return page
 
@@ -65,29 +73,87 @@ def api_framework():
 	page['meta']['error'] = error
 	return jsonify(page)
 
+@app.route('/api/iris_cluster')
+def api_iris_cluster():
+	page = {'meta': {'error': None}, 'output': {}}
+	args_dict = request.args.to_dict()
+
+	module_name = 'iris'
+	# Validating and Setting framework options for Iris Module	
+	framework_option_error= set_framework_options(module_name, args_dict) 
+	if(framework_option_error):
+		page['meta']['error'] = framework_option_error
+		return jsonify(page)
+	# Executing Iris Module 
+	iris_search_result = run_module_api(module_name)
+	if('error' in iris_search_result.keys()):
+		page['meta']['error'] = iris_search_result.get('error', None)
+		return jsonify(page)
+	page['output']['iris_search_result'] = iris_search_result.get('output', None)	
+	
+	module_name = 'cluster'
+	# Validating and Setting framework options for Cluster Module	
+	cluster_user_options = {
+		'data': dumps(iris_search_result.get('output', None))
+	}
+	framework_option_error= set_framework_options(module_name, cluster_user_options) 
+	if(framework_option_error):
+		page['meta']['error'] = framework_option_error
+		return jsonify(page)
+	# Executing Cluster Module 
+	cluster_result = run_module_api(module_name)
+	page['output']['cluster_result'] = cluster_result.get('output', None)
+	return jsonify(page)
+
+
+
+
 @app.route('/api/modules')
 def api_modules():
 	page = {'meta': {'error': None, 'command': None}, 'output': {}}
+
 	# If no module specified
 	args_dict = request.args.to_dict()
 	if '_module' not in args_dict:
 		page['meta']['error'] = 'No module specified.'
 		return jsonify(page)
+
 	module_name = args_dict.pop('_module')
 	# If module doesn't exist
 	if module_name not in framework._loaded_modules:
 		page['meta']['error'] = f"Module name '{module_name}' not found."
 		return jsonify(page)
-	if args_dict == {}:
-		page['meta']['error'] = f"No option specified."
+	
+	# Validating and Setting framework options	
+	framework_option_error= set_framework_options(module_name, args_dict) 
+	if(framework_option_error):
+		page['meta']['error'] = framework_option_error
 		return jsonify(page)
+
+	# Executing Module
+	result = run_module_api(module_name)
+	page['meta']['error'] = result.get('error', None)
+	page['output'] = result.get('output', None)
+	page['meta']['command'] = result.get('command', None)
+	return jsonify(page)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return "<pre>404</pre>", 404
+
+
+def set_framework_options(module_name, user_options):
+	if user_options == {}:
+		return f"No option specified."
+	
 	module = framework._loaded_modules[module_name]
 	options = module.meta['options']
 	true_options = ('true', 'on', 'yes', '1', True)
 	framework.options = {}
+
 	# Add framework options
-	if 'output' in args_dict:
-		if args_dict['output'] in true_options:
+	if 'output' in user_options:
+		if user_options['output'] in true_options:
 			framework.options['output'] = True
 		else:
 			framework.options['output'] = False
@@ -97,43 +163,45 @@ def api_modules():
 	# Setting options
 	for option in options:
 		option_name = option[0]
-		option_required = option[2]
+		default_option_value = option[1]
+		option_required = option[2] # OPTION REQUIRED CHECK
 		option_type = option[6]
 		option_name_short = option[4][1:]
 		option_action = option[5]
-		if option_name in args_dict:
-			option_value = args_dict[option_name]
-		elif option_name_short in args_dict:
-			option_value = args_dict[option_name_short]
+		if option_name in user_options:
+			option_value = user_options[option_name]
+		elif option_name_short in user_options:
+			option_value = user_options[option_name_short]
 		else:
-			option_value = option[1]
+			option_value = default_option_value
 
 		if option_action == 'store':
-			if isinstance(option_value, option_type):
+			if option_value == default_option_value or isinstance(option_value, option_type):
 				framework.options[option_name] = option_value
 			else:
-				page['meta']['error'] = f"Need {option_type}. got invalid type for {option_name}."
-				return jsonify(page)
+				return f"Need {option_type}. got invalid type for {option_name}."
 		else:
 			if option_value in true_options:
 				framework.options[option_name] = True
+	
+
+
+def run_module_api(module_name):
+	result = {}
 	try:
 		output = framework.mod_api_run(module_name)
 	except Exception as e:
 		framework.print_exception()
 		output = False
 	if output == False:
-		page['meta']['error'] = 'Something went wrong.'
+		result['error'] = 'Something went wrong'
 	else:
-		page['output'] = output
-		if page['output']['running_errors'] != []:
-			page['meta']['error'] = 'Runtime error.'
-	page['meta']['command'] = framework.options
-	return jsonify(page)
-
-@app.errorhandler(404)
-def page_not_found(e):
-    return "<pre>404</pre>", 404
+		result['output'] = output
+		if result['output']['running_errors'] != []:
+			result['error'] = 'Runting error'
+	result['command'] = framework.options 
+	return result
+	
 
 def run_app(core_obj, host='127.0.0.1', port=1313):
 	global framework
