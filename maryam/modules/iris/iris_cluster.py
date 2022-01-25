bcba80299a4802986f433f95d6ce0f9864781d16
Saeed Dehqan <31902891+saeeddhqan@users.noreply.github.com>
2022-01-25 06:19:40 -0500

    Update iris_cluster.py

--- maryam/modules/iris/iris_cluster.py
+++ maryam/modules/iris/iris_cluster.py
@@ -13,53 +13,41 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 meta = {
-    'name': 'Iris_Cluster',
-    'author': 'Shaad',
-    'version': '0.1',
-    'description': 'Get Iris Search result and clustered results for your query',
-        'required': ('Same as Iris and Cluster module'),
-        'options': (
-            ('query', None, True, 'Query string', '-q', 'store', str),
-        ),
-    'examples': ('iris_cluster -q <QUERY>')
+	'name': 'Iris_Cluster',
+	'author': 'Shaad',
+	'version': '0.1',
+	'description': 'Get Iris Search result and clustered results for your query',
+	'required': ('kneed', 'mlxtend'),
+	'options': (
+			('query', None, True, 'Query string', '-q', 'store', str),
+		),
+	'examples': ('iris_cluster -q <QUERY>')
 }
 
 
 def module_api(self):
-    query = self.options['query']
-    output_option_value = self.options['output']
-    output = {}
+	query = self.options['query']
+	output_option_value = self.options['output']
+	output = {}
+	mode = self._mode
+	# Computing iris search result
+	self._mode = 'api_mode'
+	iris_search_result = self.opt_proc('iris', args=f'-q "{query}" --api', output='web_api')
+	# Computing cluster results
+	clusterer = self.cluster(iris_search_result)
+	output = {'results': clusterer.perform_clustering()}
 
-    # Computing iris search result
-    self._mode = 'api_mode'
-    iris_search_result = self.opt_proc('iris', args=f'-q "{query}" --api', output='web_api')
-    output['iris_search_result'] = iris_search_result
+	self._mode = mode
+	self.save_gather(output, 'iris/iris_cluster', query, output=self.options['output'])
 
-    # Computing cluster results
-    clusterer = self.cluster(iris_search_result)
-    output['cluster_result'] = {'json': clusterer.perform_clustering()}
-
-    # Resetting options for iris_cluster_module
-    self.options = {}
-    self.options['query'] = query
-    self.options['output'] = output_option_value
-
-    self.save_gather(output, 'iris/iris_cluster', query, output=self.options['output'])
-    
-    return output
-
+	return output
 
 def module_run(self):
-    output = module_api(self)
-    iris_search_result = output['iris_search_result']
-    cluster_result = output['cluster_result']['json']
+	output = module_api(self)
 
-    print('IRIS SEARCH RESULT: ')
-    self.search_engine_results(iris_search_result)
-
-    print('\n\nCLUSTER RESULT: ')
-    for index, title in enumerate(cluster_result):
-        print('\n')
-        print(f"CLUSTER {index+1}")
-        print(f"TITLE: {title}")
-        print('  '+'\n  '.join(cluster_result[title]))
+	print('\n\nCLUSTER RESULT: ')
+	for index, title in enumerate(cluster_result):
+	    print('\n')
+	    print(f"CLUSTER {index+1}")
+	    print(f"TITLE: {title}")
+	    print('  '+'\n  '.join(cluster_result[title]))
