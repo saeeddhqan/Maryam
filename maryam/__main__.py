b198acdc864375743fbbd2ac45fe844c95188557
Saeed Dehqan <31902891+saeeddhqan@users.noreply.github.com>
2022-06-27 08:42:13 -0400

    change version value

--- maryam/__main__.py
+++ maryam/__main__.py
@@ -16,7 +16,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-__VERSION__ = '2.5.0'
+__VERSION__ = '2.5.1'
 
 import sys
 
