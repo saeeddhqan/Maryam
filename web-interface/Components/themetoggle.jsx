fad8ee2b9328ee360977f2bd387444a6bca036bc
jailbreakerVC <54243183+jailbreakerVC@users.noreply.github.com>
2023-03-25 00:04:46 +0530

    added darktheme toggle button

--- web-interface/Components/themetoggle.jsx
+++ web-interface/Components/themetoggle.jsx
@@ -8,7 +8,21 @@ export default function Themetoggle({ theme, setTheme }) {
   };
   return (
     <div className="theme-toggle">
-      <button onClick={toggleTheme}>Toggle Theme</button>
+      <div className="toggle-wrap">
+        <button className="toggle-button" onClick={toggleTheme}>
+          <svg
+            viewBox="0 0 24 24"
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+          >
+            <path
+              d="M12,22 C17.5228475,22 22,17.5228475 22,12 C22,6.4771525 17.5228475,2 12,2 C6.4771525,2 2,6.4771525 2,12 C2,17.5228475 6.4771525,22 12,22 Z M12,20.5 L12,3.5 C16.6944204,3.5 20.5,7.30557963 20.5,12 C20.5,16.6944204 16.6944204,20.5 12,20.5 Z"
+              id="🎨-Color"
+            ></path>
+          </svg>
+        </button>
+      </div>
     </div>
   );
 }
