b5586c05605ce63ef8c167187a29b8a5f2a1a0d8
Moksh45 <mokshgupta567@gmail.com>
2023-06-09 10:28:50 +0530

    add setup command for node and npm

--- setup.py
+++ setup.py
@@ -15,52 +15,80 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
-
+import subprocess
 from setuptools import setup, find_packages
 
+# Check if Node.js and npm are installed
+try:
+    subprocess.run(["node", "-v"], check=True)
+    subprocess.run(["npm", "-v"], check=True)
+except FileNotFoundError:
+    print("Node.js or npm is not installed. Installing Node.js...")
+    subprocess.run(
+        ["sudo", "apt", "install", "nodejs", "-y"]
+    )  # Modify the package manager command based on your Linux distribution
+    print("Node.js installed successfully.")
+
+# Rest of the setup() function remains the same
+
 setup(
-	name='maryam',
-	version='2.5.1-3',
-	url='https://github.com/saeeddhqan/Maryam',
-	author='Saeed Dehqan',
-	author_email='saeed.dehghan@owasp.org',
-	packages=find_packages(),
-	include_package_data=True,
-	package_data={"maryam": ['data/*']},
-	license='GPL-V3',
-	description='OWASP Maryam is a modular/optional open-source framework based on OSINT and data gathering.',
-	long_description=open('README.md').read(),
-	long_description_content_type='text/markdown',
-	keywords=['OWASP', 'OSINT', 'search-engine', 'social-networks', 'Maryam'],
-	scripts=['bin/maryam'],
-	install_requires=[
-        'requests',
-        'cloudscraper',
-        'bs4',
-        'lxml',
-        'flask',
-        'vaderSentiment',
-        'plotly',
-        'nltk',
-        'matplotlib',
-        'pandas',
-        'wordcloud',
-        'numpy',
-        'dask',
-        'scikit-learn',
-        'scipy',
-        'umap',
-        'bertopic',
-        'sentence_transformers',
-        'gensim',
-        'top2vec'
+    name="maryam",
+    version="2.5.1-3",
+    url="https://github.com/saeeddhqan/Maryam",
+    author="Saeed Dehqan",
+    author_email="saeed.dehghan@owasp.org",
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={"maryam": ["data/*"]},
+    license="GPL-V3",
+    description="OWASP Maryam is a modular/optional open-source framework based on OSINT and data gathering.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    keywords=["OWASP", "OSINT", "search-engine", "social-networks", "Maryam"],
+    scripts=["bin/maryam"],
+    install_requires=[
+        "requests",
+        "cloudscraper",
+        "bs4",
+        "lxml",
+        "flask",
+        "vaderSentiment",
+        "plotly",
+        "nltk",
+        "matplotlib",
+        "pandas",
+        "wordcloud",
+        "numpy",
+        "dask",
+        "scikit-learn",
+        "scipy",
+        "umap",
+        "bertopic",
+        "sentence_transformers",
+        "gensim",
+        "top2vec",
     ],
-	classifiers=[
-		'Programming Language :: Python :: 3.10',
-		'Development Status :: 5 - Production/Stable',
-		'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-		'Natural Language :: English',
-		'Operating System :: POSIX :: Linux',
-		'Environment :: Console',
-	]
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Natural Language :: English",
+        "Operating System :: POSIX :: Linux",
+        "Environment :: Console",
+    ],
 )
+
+# Check if the current path exists and contains the /bin/local/web folder
+try:
+    subprocess.run(["ls", "./maryam/core/web/webSearchInterface"], check=True)
+except FileNotFoundError:
+    print(
+        "The specified path does not exist or does not contain the /bin/local/web folder."
+    )
+    exit(1)
+
+# Install npm packages in the specified path
+print("Installing npm packages...")
+subprocess.run(["npm", "install"], cwd="./maryam/core/web/webSearchInterface")
+print("npm packages installed successfully.")
+
